eacf43f6c1ed664073b17b4586a0c81a94db3f7c
MartijnW <weningmartijn@gmail.com>
2024-09-20 12:36:17 +0200

    database added

--- CODE/WebApplication1/Program.cs
+++ CODE/WebApplication1/Program.cs
@@ -1,3 +1,5 @@
+using WebApplication1.Models;
+
 namespace WebApplication1
 {
     public class Program
@@ -8,6 +10,7 @@ namespace WebApplication1
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
+            builder.Services.AddDbContext<FrietzaakDBContext>();
 
             var app = builder.Build();
 
